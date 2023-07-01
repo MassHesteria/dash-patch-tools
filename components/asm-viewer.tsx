46d6e31f6ec34a90997a23b084d14a077b692aec
MassHesteria <brad.hester@gmail.com>
2023-07-01 11:15:34 -0500

    ASM viewer background color

--- components/asm-viewer.tsx
+++ components/asm-viewer.tsx
@@ -2,8 +2,14 @@ import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import gruvbox from "react-syntax-highlighter/dist/esm/styles/prism/gruvbox-dark";
 
 export function AsmViewer({ children }: { children: any }) {
+  //TODO: This feels ugly
+  const bgColor = gruvbox[':not(pre) > code[class*="language-"]']
+    .background as string;
+  const bgStyle = {
+    backgroundColor: bgColor,
+  };
   return (
-    <div className="h-screen overflow-auto">
+    <div className="h-screen overflow-auto" style={bgStyle}>
       <SyntaxHighlighter language="asm6502" style={gruvbox}>
         {children}
       </SyntaxHighlighter>
